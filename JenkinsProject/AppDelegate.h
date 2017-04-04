87e09e5a97bd9583801063177c127fe89223587c
selvamca45 <selvamca45@gmail.com>
2017-04-04 14:03:22 +0530

    Project files added
    Jenkins project files are added

---
+++ JenkinsProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsProject
+//
+//  Created by Administrator on 04/04/17.
+//  Copyright © 2017 Selvakumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
